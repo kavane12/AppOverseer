3847d04ee1287d8fab0ab090f799995f70683ebf
kavane12 <gekevin1998@gmail.com>
2020-01-27 20:24:21 -0800

    Add 'Other' tab to bottom nav

--- app/src/main/java/com/example/appoverseer/NavActivity.kt
+++ app/src/main/java/com/example/appoverseer/NavActivity.kt
@@ -16,13 +16,15 @@ class NavActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
                 R.id.navigation_dashboard,
-                R.id.navigation_notifications
+                R.id.navigation_notifications,
+                R.id.navigation_other
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
