c7fcd04504ee84eaceb87dc9466cc3b5d1474183
kavane12 <gekevin1998@gmail.com>
2020-03-09 18:09:50 -0700

    Added Google Fit API and permission asking

--- app/src/main/java/com/example/planmyworkout/MainActivity.kt
+++ app/src/main/java/com/example/planmyworkout/MainActivity.kt
@@ -1,23 +1,90 @@
 package com.example.planmyworkout
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
+
 const val RC_SIGN_IN = 7
 const val USER = "com.example.planmyworkout.CURRENT_USER"
+
+const val PERMISSION_ALL = 1
+val permissions = arrayOf(
+    Manifest.permission.ACTIVITY_RECOGNITION,
+    Manifest.permission.ACCESS_FINE_LOCATION
+)
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        // Checks and ask for user permissions
+        if (gottenAllPermissions()) {
+            // Prompt login if have all permissions
+            signInUser()
+        }
+    }
 
+
+    // PERMISSIONS FUNCTIONS
+
+    private fun hasPermissions(context: Context, vararg permissions: String): Boolean = permissions.all {
+        ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun gottenAllPermissions(): Boolean {
+        if (!hasPermissions(this, *permissions)) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                permissions, PERMISSION_ALL);
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        var accepted = true
+
+        when (requestCode) {
+            PERMISSION_ALL -> {
+                for (i in 0 until permissions.count()) {
+                    if (grantResults.isNotEmpty() && grantResults[i] == PackageManager.PERMISSION_DENIED)
+                    {
+                        accepted = false
+                        break
+                    }
+                }
+            }
+        }
+
+        if (accepted) {
+            // All permissions granted, show login
+            signInUser()
+        }
+    }
+
+
+    // LOG IN FUNCTIONS
+
+    private fun signInUser() {
         val user = FirebaseAuth.getInstance().currentUser
 
         // If user is already logged in, go to home page, else prompt login
@@ -59,6 +126,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    // FINALLY DONE FUNCTION
 
     private fun switchToHomePage(user: FirebaseUser?) {
         // Switch to home page and end current activity
